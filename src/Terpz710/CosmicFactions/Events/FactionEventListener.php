773f69f449d163014dab33572c0282f09565308d
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2024-05-17 08:48:43 -0700

    updates

--- src/Terpz710/CosmicFactions/Events/FactionEventListener.php
+++ src/Terpz710/CosmicFactions/Events/FactionEventListener.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Terpz710\CosmicFactions\Events;
 
 use pocketmine\event\Listener;
+use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\player\Player;
 use Terpz710\CosmicFactions\FactionManager;
@@ -17,13 +18,21 @@ class FactionEventListener implements Listener {
         $this->factionManager = $factionManager;
     }
 
-    public function onEntityDamage(EntityDamageByEntityEvent $event): void {
+    public function onEntityDamage(EntityDamageEvent $event): void {
         $entity = $event->getEntity();
-        $damager = $event->getDamager();
 
-        if ($entity instanceof Player && $damager instanceof Player) {
+        if ($entity instanceof Player) {
             if ($entity->getHealth() - $event->getFinalDamage() <= 0) {
-                $this->handlePlayerKill($entity, $damager);
+                if ($event instanceof EntityDamageByEntityEvent) {
+                    $damager = $event->getDamager();
+                    if ($damager instanceof Player) {
+                        $this->handlePlayerKill($entity, $damager);
+                    } else {
+                        $this->handlePlayerDeath($entity);
+                    }
+                } else {
+                    $this->handlePlayerDeath($entity);
+                }
             }
         }
     }
@@ -32,15 +41,26 @@ class FactionEventListener implements Listener {
         $victimFaction = $this->factionManager->getFaction($victim);
         $killerFaction = $this->factionManager->getFaction($killer);
 
-        if ($victimFaction !== null && $killerFaction !== null) {
-            $victimPower = $this->factionManager->getFactionPower($victimFaction);
-            $killerPower = $this->factionManager->getFactionPower($killerFaction);
+        if ($victimFaction !== null) {
+            $victimPowerLoss = 1; //victim
+            $this->factionManager->reduceFactionPower($victimFaction, $victimPowerLoss);
+            $victim->sendMessage("You have lost $victimPowerLoss power due to being killed by {$killer->getName()}!");
+        }
 
-            $this->factionManager->reduceFactionPower($victimFaction, $victimPower);
-            $this->factionManager->addFactionPower($killerFaction, $victimPower);
+        if ($killerFaction !== null) {
+            $killerPowerGain = 1; //killer
+            $this->factionManager->addFactionPower($killerFaction, $killerPowerGain);
+            $killer->sendMessage("You have gained $killerPowerGain power for killing {$victim->getName()}!");
+        }
+    }
 
-            $victim->sendMessage("You have lost $victimPower power!");
-            $killer->sendMessage("You have gained $victimPower power!");
+    private function handlePlayerDeath(Player $victim): void {
+        $victimFaction = $this->factionManager->getFaction($victim);
+
+        if ($victimFaction !== null) {
+            $victimPowerLoss = 1; //default
+            $this->factionManager->reduceFactionPower($victimFaction, $victimPowerLoss);
+            $victim->sendMessage("You have lost $victimPowerLoss power!");
         }
     }
 }
